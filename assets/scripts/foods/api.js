e1fcddf7a295d62fad8fd2d527f9efc36c090c72
Emily Deadman <emilydeadman@Emilys-MacBook-Pro.local>
2020-09-16 13:26:27 -0400

    styling updates

--- assets/scripts/foods/api.js
+++ assets/scripts/foods/api.js
@@ -1,58 +1,58 @@
-// Require:
-const config = require('../config')
-const store = require('../store')
+// // Require:
+// const config = require('../config')
+// const store = require('../store')
 
-// AJAX:
+// // AJAX:
 
-const createFood = function (formData) {
-  return $.ajax({
-    headers: {
-      Authorization: 'Bearer ' + store.user.token
-    },
-    url: config.apiUrl + '/foods',
-    method: 'POST',
-    user: store.user,
-    data: formData
-  })
-}
+// const createFood = function (formData) {
+//   return $.ajax({
+//     headers: {
+//       Authorization: 'Bearer ' + store.user.token
+//     },
+//     url: config.apiUrl + '/foods',
+//     method: 'POST',
+//     user: store.user,
+//     data: formData
+//   })
+// }
 
-const indexFoods = function () {
-  return $.ajax({
-    headers: {
-      Authorization: 'Bearer ' + store.user.token
-    },
-    url: config.apiUrl + '/foods',
-    method: 'GET',
-    data: {
-      food: []
-    }
-  })
-}
+// const indexFoods = function () {
+//   return $.ajax({
+//     headers: {
+//       Authorization: 'Bearer ' + store.user.token
+//     },
+//     url: config.apiUrl + '/foods',
+//     method: 'GET',
+//     data: {
+//       food: []
+//     }
+//   })
+// }
 
-const destroyFood = function (foodId) {
-  return $.ajax({
-    headers: {
-      Authorization: 'Bearer ' + store.user.token
-    },
-    url: config.apiUrl + '/foods/' + foodId,
-    method: 'DELETE'
-  })
-}
+// const destroyFood = function (foodId) {
+//   return $.ajax({
+//     headers: {
+//       Authorization: 'Bearer ' + store.user.token
+//     },
+//     url: config.apiUrl + '/foods/' + foodId,
+//     method: 'DELETE'
+//   })
+// }
 
-const updateFood = function (foodId, formData) {
-  return $.ajax({
-    headers: {
-      Authorization: 'Bearer ' + store.user.token
-    },
-    url: config.apiUrl + '/foods/' + foodId,
-    method: 'PATCH',
-    data: formData
-  })
-}
+// const updateFood = function (foodId, formData) {
+//   return $.ajax({
+//     headers: {
+//       Authorization: 'Bearer ' + store.user.token
+//     },
+//     url: config.apiUrl + '/foods/' + foodId,
+//     method: 'PATCH',
+//     data: formData
+//   })
+// }
 
-module.exports = {
-  createFood,
-  indexFoods,
-  destroyFood,
-  updateFood
-}
+// module.exports = {
+//   createFood,
+//   indexFoods,
+//   destroyFood,
+//   updateFood
+// }
