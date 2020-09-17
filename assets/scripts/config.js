cc070640f906c6e24e1e10b680d3a0a3883a5a39
Emily Deadman <emilydeadman@Emilys-MacBook-Pro.local>
2020-09-17 18:05:41 -0400

    fixed // that was causing an error (heroku link)

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl;
 const apiUrls = {
-  production: "https://enigmatic-fortress-92738.herokuapp.com/",
+  production: "https://enigmatic-fortress-92738.herokuapp.com",
   development: "http://localhost:4741",
 };
 
